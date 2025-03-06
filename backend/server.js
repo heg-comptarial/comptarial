1c13782dded4b3778ba6663a9833b7c634f41111
Boran Uzun <boran.u3@gmail.com>
2025-03-06 17:20:55 +0100

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
-import express from "express";
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
-const port = 3000;
+app.use(cors());
+app.use(express.json());
 
-app.get("/hello", (req, res) => {
-  res.send("Hello World!");
+// Route test t
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from backend!" });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+const PORT = 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
