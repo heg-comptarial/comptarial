26790f93f963aaa3fcced950726ae3c2dac4ac68
Hkahriman <hakija.kahrimanovic@etu.hesge.ch>
2025-03-20 14:43:01 +0100

    ajouts de package pour docker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import path from 'path';
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { isServer }) {
+    // Résoudre l'alias @ vers le dossier src
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'), // Cela doit correspondre à la structure de ton projet
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
