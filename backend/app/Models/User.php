e41e0cf9209d34bcbc70213f1748c75f3ab4b89b
khloud15 <khokha152010@gmail.com>
2025-04-01 23:49:34 +0200

    Créer les Factories

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -39,7 +39,7 @@ use Laravel\Sanctum\HasApiTokens;
  */
 class User extends Authenticatable
 {
-	use HasApiTokens, Notifiable;
+	use HasApiTokens, Notifiable, HasFactory;
 	protected $table = 'user';
 	protected $primaryKey = 'user_id';
 	public $timestamps = false;
