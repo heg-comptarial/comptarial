1d0ef6ab42f2fa67c6caaf2ad496bced94e72704
khloud15 <khokha152010@gmail.com>
2025-04-02 00:06:42 +0200

    Prive Factory

--- backend/database/factories/PriveFactory.php
+++ backend/database/factories/PriveFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Prive;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Prive>
@@ -17,7 +19,21 @@ class PriveFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(), // Associe un utilisateur à la table "prive"
+            'dateNaissance' => $this->faker->dateOfBirth, // Génère une date de naissance aléatoire
+            'nationalite' => $this->faker->country, // Génère une nationalité aléatoire
+            'etatCivil' => $this->faker->randomElement(['Célibataire', 'Marié', 'Divorcé', 'Veuf']), // Génère un état civil aléatoire
+            'fo_banques' => $this->faker->boolean, // Génère une valeur booléenne aléatoire
+            'fo_dettes' => $this->faker->boolean,
+            'fo_immobiliers' => $this->faker->boolean,
+            'fo_salarie' => $this->faker->boolean,
+            'fo_autrePersonneCharge' => $this->faker->boolean,
+            'fo_independant' => $this->faker->boolean,
+            'fo_rentier' => $this->faker->boolean,
+            'fo_autreRevenu' => $this->faker->boolean,
+            'fo_assurance' => $this->faker->boolean,
+            'fo_autreDeduction' => $this->faker->boolean,
+            'fo_autreInformations' => $this->faker->boolean,
         ];
     }
 }
