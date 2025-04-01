2b417144abf1c09373ffe0664328864caf373d91
khloud15 <khokha152010@gmail.com>
2025-04-01 23:52:42 +0200

    Administrateur Factory

--- backend/database/factories/AdministrateurFactory.php
+++ backend/database/factories/AdministrateurFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Administrateur;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Administrateur>
@@ -17,7 +19,8 @@ class AdministrateurFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(), // Crée un utilisateur lié à cet administrateur
+            'niveauAcces' => $this->faker->randomElement(['admin', 'superadmin', 'moderator']), // Exemple de valeurs pour niveauAcces
         ];
     }
 }
