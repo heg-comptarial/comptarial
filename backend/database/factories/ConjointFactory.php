6c039924a43c22d5064bbde6fa77490dac10539f
khloud15 <khokha152010@gmail.com>
2025-04-02 00:12:58 +0200

    Conjoint Factory

--- backend/database/factories/ConjointFactory.php
+++ backend/database/factories/ConjointFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Conjoint;
+use App\Models\Prive;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Conjoint>
@@ -17,7 +20,15 @@ class ConjointFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'prive_id' => Prive::factory(), // Associe un conjoint à un utilisateur privé existant
+            'nom' => $this->faker->lastName(), // Génère un nom de famille
+            'prenom' => $this->faker->firstName(), // Génère un prénom
+            'nationalite' => $this->faker->country(), // Génère une nationalité
+            'dateNaissance' => $this->faker->dateOfBirth(), // Génère une date de naissance
+            'localite' => $this->faker->city(), // Génère une ville
+            'adresse' => $this->faker->address(), // Génère une adresse
+            'codePostal' => $this->faker->postcode(), // Génère un code postal
+            'situationProfessionnelle' => $this->faker->jobTitle(), // Génère une situation professionnelle
         ];
     }
 }
