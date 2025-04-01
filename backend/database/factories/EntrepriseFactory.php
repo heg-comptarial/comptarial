924548551f24ef359dcc9765099e57705dc89b01
khloud15 <khokha152010@gmail.com>
2025-04-02 00:03:02 +0200

    Entreprise Factory

--- backend/database/factories/EntrepriseFactory.php
+++ backend/database/factories/EntrepriseFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Entreprise;
+use App\Models\User;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Entreprise>
@@ -17,7 +20,12 @@ class EntrepriseFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(), // Associe un utilisateur à l'entreprise
+            'raisonSociale' => $this->faker->company, // Génère une raison sociale (nom de l'entreprise)
+            'prestations' => $this->faker->sentence, // Génère une prestation aléatoire
+            'grandLivre' => $this->faker->word, // Génère un grand livre fictif
+            'numeroFiscal' => $this->faker->unique()->numerify('FR###########'), // Génère un numéro fiscal fictif
+            'nouvelleEntreprise' => $this->faker->boolean, // Génère un booléen pour savoir si l'entreprise est nouvelle
         ];
     }
 }
