8b84712be4b2b4f7b59a597c45ba4b0b42f7b556
khloud15 <khokha152010@gmail.com>
2025-04-02 00:04:47 +0200

    Notification factory

--- backend/database/factories/NotificationFactory.php
+++ backend/database/factories/NotificationFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Notification;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Notification>
@@ -17,7 +19,9 @@ class NotificationFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+           'user_id' => User::factory(), // Associe un utilisateur à la notification
+            'contenu' => $this->faker->sentence, // Contenu de la notification (phrase aléatoire)
+            'dateCreation' => $this->faker->dateTimeThisYear, // Génère une date aléatoire cette année
         ];
     }
 }
