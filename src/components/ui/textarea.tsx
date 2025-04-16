f7c00376daca0730030a10e9119a0019531bda64
khloud15 <khokha152010@gmail.com>
2025-04-16 04:11:08 +0200

    create textarea.tsx

---
+++ src/components/ui/textarea.tsx
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+
+export const Textarea = React.forwardRef<
+  HTMLTextAreaElement,
+  React.TextareaHTMLAttributes<HTMLTextAreaElement>
+>(({ className, ...props }, ref) => (
+  <textarea
+    ref={ref}
+    className={`border rounded-md p-2 ${className}`}
+    {...props}
+  />
+));
+Textarea.displayName = 'Textarea';
